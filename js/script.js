1262160c3a4cb345ca07ac08bc362a7816ffe28f
fahimrimon <fahimrimon21@gmail.com>
2022-03-02 15:08:19 +0600

    display search

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// search phone
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -7,5 +8,26 @@ const searchPhone = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+
+// search phone result
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                 <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Model: ${phone.phone_name}</h5>
+                    <h5 class="card-title">Brand: ${phone.brand}</h5>
+                    <button type="button" class="btn btn-primary">More Details</button>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
