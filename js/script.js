08f5e577016034973141852ebe8a018ab0c4b9f7
fahimrimon <fahimrimon21@gmail.com>
2022-03-02 17:03:32 +0600

    load search result

--- js/script.js
+++ js/script.js
@@ -19,15 +19,28 @@ const displaySearchResult = phones => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card h-100">
+            <div class="card h-50 w-75 mx-auto">
                  <img src="${phone.image}" class="card-img-top" alt="...">
                     <div class="card-body">
                     <h5 class="card-title">Model: ${phone.phone_name}</h5>
                     <h5 class="card-title">Brand: ${phone.brand}</h5>
-                    <button type="button" class="btn btn-primary">More Details</button>
+                    <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary">More Details</button>
                 </div>
             </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+// Load Phone Deatail
+const loadPhoneDetail = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data));
+}
+
+// Display Phone Details
+const displayPhoneDetail = phone => {
+
 }
